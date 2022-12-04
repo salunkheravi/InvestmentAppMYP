2d33177975a142733a736f8deb1018629cfdbb66
salunkheravi <ravi@qitplus.com>
2022-12-04 17:48:33 +1000

    Implemented Repository

--- API/Data/InvestmentRepository.cs
+++ API/Data/InvestmentRepository.cs
@@ -1,7 +1,45 @@
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace API.Data
 {
-    public class InvestmentRepository
+    public class InvestmentRepository : IInvestmentRepository
     {
-        
+        private readonly DataContext _context;        
+        public InvestmentRepository(DataContext context)
+        {            
+            _context = context;
+        }
+
+        public async Task AddInvestmentAsync(Investment investment)
+        {
+            await _context.Investments.AddAsync(investment);  
+        }
+
+        public void DeleteInvestment(Investment investment)
+        {
+            _context.Investments.Remove(investment);
+        }
+
+        public async Task<Investment> GetInvestmentAsync(string name)
+        {
+            return await _context.Investments.FindAsync(name);
+        }
+
+        public async Task<IEnumerable<Investment>> GetInvestmentsAsync()
+        {
+            return await _context.Investments.ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void UpdateInvestment(Investment investment)
+        {
+            _context.Entry(investment).State = EntityState.Modified;
+        }
     }
 }
--- API/Interfaces/IInvestmentRepository.cs
+++ API/Interfaces/IInvestmentRepository.cs
@@ -1,7 +1,15 @@
+using API.Entities;
+
 namespace API.Interfaces
 {
     public interface IInvestmentRepository
     {
-        
+       Task<bool> SaveAllAsync();
+       Task<IEnumerable<Investment>> GetInvestmentsAsync();    
+       Task<Investment> GetInvestmentAsync(string name);  
+       Task AddInvestmentAsync(Investment investment);       
+       void UpdateInvestment(Investment investment);
+       void DeleteInvestment(Investment investment);
+
     }
 }

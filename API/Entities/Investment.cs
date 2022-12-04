cb03fb0a100fd5b4e2840e066d2603b792ae7671
salunkheravi <ravi@qitplus.com>
2022-12-04 18:42:57 +1000

    implemented simple interest logic

--- API/Entities/Investment.cs
+++ API/Entities/Investment.cs
@@ -13,7 +13,17 @@ namespace API.Entities
        public double GetCurrentValue()
        {
             double currentValue = 0.0;
-            return currentValue;
+            var periodInMonths = Math.Round(DateTime.Now.Subtract(this.StartDate).Days / (365.2425 / 12));
+            if(this.InterestType.ToLower() == "simple")
+            { 
+                //Calculating the simple interest based on the information provided on 
+                //https://www.calculatorsoup.com/calculators/financial/simple-interest-plus-principal-calculator.php
+                currentValue = this.Principal * (1 + ((this.InterestRate/100 ) * (periodInMonths/12)));
+                
+            }else if(this.InterestType.ToLower() == "compound")
+            {
+            }
+            return Math.Round(currentValue,2);
        }
     }
 }
